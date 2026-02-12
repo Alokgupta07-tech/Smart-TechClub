1f3f860d470e765f881c408f15bb2157df11a872
agupta88094 <agupta88094@gmail.com>
2026-02-12 22:30:19 +0530

    fix: remove puzzles rewrite, rely on automatic routing

--- api/puzzles.js
+++ api/puzzles.js
@@ -5,6 +5,9 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.status(200).end();
+  
+  // Debug checkpoint - return immediately to test
+  // return res.status(200).json({ debug: 'handler reached', method: req.method });
 
   try {
     const { getSupabase } = require('./_lib/supabase');
